8bfa5400678a0e7b0aa8a4c98dddd853db7f53ae
Logvinov_fv <logvinov_fv@ursip.ru>
2019-08-15 19:58:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	app = express(),
+	methodOverride = require('method-override'),
+	port = process.env.PORT || 5010,
+	keys = require('./config/keys')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+const mongoDB = keys.MONGODB_URI
+
+mongoose.Promise = global.Promise
+//connect to mongoose
+mongoose
+	.connect(mongoDB, {useNewUrlParser: true})
+	.then(() => console.log('MongoDB connected!'))
+	.catch(err => console.log('error connecting to MongoDB\n', err))
+
+app.listen(port, function() {
+	console.log('server up and running on port', port)
+})
